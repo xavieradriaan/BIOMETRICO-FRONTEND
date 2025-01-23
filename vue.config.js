33eacc136e2ad3d888ba760253a5318eaaee393d
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2025-01-22 21:42:05 -0500

    tercerCommit

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,9 @@
 const { defineConfig } = require('@vue/cli-service')
+
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    disableHostCheck: true,
+    public: 'biometrico-frontend-production.up.railway.app'
+  }
 })
