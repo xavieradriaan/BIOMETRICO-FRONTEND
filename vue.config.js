423f208cfd072a315f23d63f91c135d505940310
Xavier Andrade <xavierandrade@MacBook-Pro-de-Xavier.local>
2025-01-22 22:07:50 -0500

    dd

--- vue.config.js
+++ vue.config.js
@@ -3,7 +3,6 @@ const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
   transpileDependencies: true,
   devServer: {
-    disableHostCheck: true,
     allowedHosts: [
       'biometrico-frontend-production.up.railway.app'
     ]
